d25a562885eed7ab60c4bbec555a041314f32e6b
muzing <muzi2001@foxmail.com>
2022-12-30 18:54:57 +0800

    Update ws01-cpp05_names_launch
    更新「通过launch文件启动节点，解决重名问题」相关代码

--- ws01_plumbing/src/cpp05_names/launch/demo01_names_launch.py
+++ ws01_plumbing/src/cpp05_names/launch/demo01_names_launch.py
@@ -4,6 +4,14 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    turtle_node = Node(package="turtlesim", executable="turtlesim_node", name="t1")
+    turtle_node_1 = Node(
+        package="turtlesim", executable="turtlesim_node", name="turtle1"
+    )
+    turtle_node_2 = Node(
+        package="turtlesim", executable="turtlesim_node", namespace="t1"
+    )
+    turtle_node_3 = Node(
+        package="turtlesim", executable="turtlesim_node", namespace="t1", name="turtle1"
+    )
 
-    return LaunchDescription([turtle_node])
+    return LaunchDescription([turtle_node_1, turtle_node_2, turtle_node_3])
