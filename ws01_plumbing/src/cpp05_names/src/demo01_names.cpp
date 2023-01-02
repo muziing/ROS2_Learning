fc428e38a475a3580ae4b347480e5681166c22ff
muzing <muzi2001@foxmail.com>
2023-01-02 15:39:50 +0800

    Update ws01-05_names
    添加「通过编程方式设置不同级别话题」相关代码

--- ws01_plumbing/src/cpp05_names/src/demo01_names.cpp
+++ ws01_plumbing/src/cpp05_names/src/demo01_names.cpp
@@ -1,12 +1,23 @@
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
 class MyNode : public rclcpp::Node
 {
 public:
-    MyNode() : Node("my_node_cpp", "t1_ns")
+    MyNode() : Node("zhen_keng", "zuoxie") // 通过编程方式设置命名空间"zuoxie""
     {
-        // 通过编程方式设置命名空间
+        // 全局话题：与命名空间、节点名称无关系
+        // pub_ = this->create_publisher<std_msgs::msg::String>("/shi", 10);
+
+        // 相对话题
+        // pub_ = this->create_publisher<std_msgs::msg::String>("kaihui", 10);
+
+        // 私有话题
+        pub_ = this->create_publisher<std_msgs::msg::String>("~/vip", 10);
     }
+
+private:
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
 };
 
 int main(int argc, char const *argv[])
--- ws01_plumbing/src/py05_names/py05_names/demo01_names_py.py
+++ ws01_plumbing/src/py05_names/py05_names/demo01_names_py.py
@@ -1,11 +1,21 @@
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import String
 
 
 class MyNode(Node):
     def __init__(self):
         # 通过编程方式设置命名空间
-        super().__init__("my_node_py", namespace="t1_ns_py")
+        super().__init__("zhenkeng_py", namespace="zuoxie_py")
+
+        # 全局话题
+        # self.pub = self.create_publisher(String, "/shi", 10)
+
+        # 相对话题
+        # self.pub = self.create_publisher(String, "kaihui", 10)
+
+        # 私有话题
+        self.pub = self.create_publisher(String, "~/vip", 10)
 
 
 def main():
